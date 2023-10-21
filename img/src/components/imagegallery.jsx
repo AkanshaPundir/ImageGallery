26ed809ca84e55aecbc1d0dbe2d40978fe20e858
AkanshaPundir <akansha.pundir2308@gmail.com>
2023-10-21 20:14:00 +0530

    3

---
+++ img/src/components/imagegallery.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import "../css/imagegallery.css"
+function ImageGallery(){
+    return(
+        <div className="gallery">
+            <h1>Image Gallery</h1>
+                <div className="photo">
+                    <span><img src="1697894062983.png" id="one"></img></span>
+                    <span><img src="1697894063010.png" id="two"></img></span>
+                    <span><img src="1697894131506.png" id="three"></img></span>
+                    <span><img src="1697894131536.png" id="four"></img></span>
+                    <span><img src="1697894149730.png" id="five"></img></span>
+                    <span><img src="1697894149753.png" id="six"></img></span>
+                    
+                
+            </div>
+            
+        </div>
+    )
+}
+
+export default ImageGallery
